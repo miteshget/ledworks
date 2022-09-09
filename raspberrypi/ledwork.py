71fcec8d550a1313b516762968ae4e98d763f7df
Mitesh Sharma <mitsharm@redhat.com>
2022-09-09 10:14:24 +0530

    update

--- raspberrypi/ledwork.py
+++ raspberrypi/ledwork.py
@@ -12,16 +12,18 @@ LED_CHANNEL = 0
 strip = Adafruit_NeoPixel(LED_COUNT,LED_PIN,LED_FREQ_HZ,LED_DMA,LED_INVERT,LED_BRIGHTNESS,LED_CHANNEL)
 strip.begin()
 
-
-for x in range(0, LED_COUNT):
-  strip.setPixelColor(x, Color(0,0,0))
-strip.show()
-for x in range(0, LED_COUNT):
-  strip.setPixelColor(x + 1, Color(255,1,1))
+m = 0
+while m < 20:
+  for x in range(0, LED_COUNT):
+    strip.setPixelColor(x, Color(0,0,0))
   strip.show()
-  time.sleep(2)
-  strip.setPixelColor(x + 2, Color(1,1,255))
-  strip.show()
-  time.sleep(2)
-  strip.setPixelColor(x + 3, Color(1,255,1))
-  strip.show()
+  for x in range(0, LED_COUNT):
+    strip.setPixelColor(x + 1, Color(255,1,1))
+    strip.show()
+    time.sleep(2)
+    strip.setPixelColor(x + 2, Color(1,1,255))
+    strip.show()
+    time.sleep(2)
+    strip.setPixelColor(x + 3, Color(1,255,1))
+    strip.show()
+  m = m+1
