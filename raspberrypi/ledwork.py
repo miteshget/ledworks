f3a75cfac151138985b9b341235bddd00da9d940
Mitesh Sharma <mitsharm@redhat.com>
2022-09-09 10:08:56 +0530

    update

--- raspberrypi/ledwork.py
+++ raspberrypi/ledwork.py
@@ -1,7 +1,7 @@
 from rpi_ws281x import * 
 import time
 
-LED_COUNT = 300
+LED_COUNT = 120
 LED_PIN = 18
 LED_FREQ_HZ = 800000
 LED_DMA = 10
@@ -9,9 +9,13 @@ LED_BRIGHTNESS = 255
 LED_INVERT = False
 LED_CHANNEL = 0
 
-strip = Adafruit_NeoPixel(LED_COUNT,LED_PIN, LED_FREQ_HZ,LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip = Adafruit_NeoPixel(LED_COUNT,LED_PIN,LED_FREQ_HZ,LED_DMA,LED_INVERT,LED_BRIGHTNESS,LED_CHANNEL)
 strip.begin()
 
+
+for x in range(0, LED_COUNT):
+  strip.setPixelColor(x, Color(0,0,0))
+strip.show()
 for x in range(0, LED_COUNT):
   strip.setPixelColor(x + 1, Color(255,1,1))
   strip.show()
