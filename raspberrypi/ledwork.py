7a6cced4ff99fdae11d5a3202563f6818e533b80
Mitesh Sharma <mitsharm@redhat.com>
2022-09-09 11:20:14 +0530

    update

--- raspberrypi/ledwork.py
+++ raspberrypi/ledwork.py
@@ -20,9 +20,9 @@ while m < 20:
   for x in range(0, LED_COUNT):
     strip.setPixelColor(x + 1, Color(255,1,1))
     strip.setBrightness(200)
-    strip.setPixelColor(x + 2, Color(1,1,255))
+    strip.setPixelColor(x + 1, Color(255,1,1))
     strip.setBrightness(100)
-    strip.setPixelColor(x + 3, Color(1,255,1))
+    strip.setPixelColor(x + 1, Color(255,1,1))
     strip.show()
     time.sleep(.1)
     for x in range(0, LED_COUNT):
