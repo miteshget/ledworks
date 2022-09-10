12ca4fc5a655534cea1fd9fefc90d3a757636468
Mitesh Sharma <mitsharm@redhat.com>
2022-09-10 12:31:08 +0530

    update

--- raspberrypi/test.py
+++ raspberrypi/test.py
@@ -5,7 +5,7 @@ import time
 from rpi_ws281x import * 
 
 # LED strip configuration:
-LED_COUNT      = 60      # Number of LED pixels.
+LED_COUNT      = 120      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
@@ -95,17 +95,35 @@ if __name__ == '__main__':
 
 	print('Press Ctrl-C to quit.')
 	while True:
-		# Color wipe animations.
-		colorWipe(strip, Color(255, 0, 0))  # Red wipe
-		colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-		colorWipe(strip, Color(0, 0, 255))  # Green wipe
-		colorWipe(strip, Color(127, 127, 127))  # White theater chase
-		colorWipe(strip, Color(127,   0,   0))  # Red theater chase
-		colorWipe(strip, Color(  0,   0, 127))  # Blue theater chase
-		# Theater chase animations.
-		theaterChase(strip, Color(127, 127, 127))  # White theater chase
-		theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-		theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+		fireworks(strip, Color(255, 0, 0))  # Red wipe
+		fireworks(strip, Color(0, 0, 255))
+		fireworks(strip, Color(0, 0, 255))
+		fireworks(strip, Color(0, 255, 0))
+		fireworks(strip, Color(0, 255, 0))
+		fireworks(strip, Color(255, 0, 0))
+		fireworks(strip, Color(255, 0, 0))
+		fireworks(strip, Color(0, 255, 255))
+		fireworks(strip, Color(0, 255, 255))
+		fireworks(strip, Color(255, 0, 255))
+		fireworks(strip, Color(255, 0, 255))
+		fireworks(strip, Color(255, 250, 0))
+		fireworks(strip, Color(255, 250, 0))
+		fireworks(strip, Color(50, 255, 255))
+		fireworks(strip, Color(50, 255, 255))
+		fireworks(strip, Color(255, 50, 150))
+		fireworks(strip, Color(255, 50, 150))
+		fireworks(strip, Color(255, 250, 50))
+		fireworks(strip, Color(255, 250, 50))
+		# colorWipe(strip, Color(255, 0, 0))  # Red wipe
+		# colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+		# colorWipe(strip, Color(0, 0, 255))  # Green wipe
+		# colorWipe(strip, Color(127, 127, 127))  # White theater chase
+		# colorWipe(strip, Color(127,   0,   0))  # Red theater chase
+		# colorWipe(strip, Color(  0,   0, 127))  # Blue theater chase
+		# # Theater chase animations.
+		# theaterChase(strip, Color(127, 127, 127))  # White theater chase
+		# theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+		# theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
 		# Rainbow animations.
 		# rainbow(strip)
 		# rainbowCycle(strip)
