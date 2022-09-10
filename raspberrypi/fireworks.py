3d635b63392229abfac82ff8b7a0dccdd3f45df1
Mitesh Sharma <mitsharm@redhat.com>
2022-09-10 14:30:24 +0530

    update

--- raspberrypi/fireworks.py
+++ raspberrypi/fireworks.py
@@ -5,7 +5,7 @@ import time
 from rpi_ws281x import * 
 
 # LED strip configuration:
-LED_COUNT      = 60      # Number of LED pixels.
+LED_COUNT      = 120      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
@@ -13,78 +13,39 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0
 
+def clean(strip):
+    for x in range(0, LED_COUNT):
+		strip.setPixelColor(x, Color(0,0,0))
+	strip.show()
 
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-	"""Wipe color across display a pixel at a time."""
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, color)
+def flare(strip, a, b, c):
+	for i in range(0, 89, 1):
+		strip.setPixelColor( i , Color(a, b, c))
+		strip.setPixelColor( i - 1, Color(round(a * .7),round( b * .7 ),round(c * .7)))
+		strip.setPixelColor( i - 2, Color(round(a * .7 * .7), round(b * .7 * .7), round(c * .7 * .7)))
+		strip.setPixelColor( i - 3, Color(round(a * .7 * .7 * .7), round(b * .7 * .7 * .7), round(c * .7 * .7 * .7)))
+		strip.setPixelColor( i - 4, Color(round(a * .7 * .7 * .7 * .7), round(b * .7 * .7 * .7 * .7), round(c * .7 * .7 * .7 * .7)))
+		strip.setPixelColor( i - 5, Color(round(a * .7 * .7 * .7 * .7 * .7), round(b * .7 * .7 * .7 * .7 * .7), round(c * .7 * .7 * .7 * .7 * .7)))
+		strip.setPixelColor( i - 6, Color(round(a * .7 * .7 * .7 * .7 * .7 * .7), round(b * .7 * .7 * .7 * .7 * .7 * .7), round(c * .7 * .7 * .7 * .7 * .7 * .7)))
 		strip.show()
-		time.sleep(wait_ms/1000.0)
-
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-	"""Movie theater light style chaser animation."""
-	for j in range(iterations):
-		for q in range(3):
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, color)
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
-
-def wheel(pos):
-	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
-
-def rainbow(strip, wait_ms=20, iterations=1):
-	"""Draw rainbow that fades across all pixels at once."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((i+j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
-
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-	"""Draw rainbow that uniformly distributes itself across all pixels."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel(((i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
-
-def theaterChaseRainbow(strip, wait_ms=50):
-	"""Rainbow movie theater light style chaser animation."""
-	for j in range(256):
-		for q in range(3):
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, wheel((i+j) % 255))
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
-
-def fireworks(strip, a, b, c, wait_ms=50,):
-	for i in range(7, 89, 1):
-    	strip.setPixelColor( i , Color(a, b, c))
-    	strip.setPixelColor( i - 1, Color(a * .7, b * .7, c * .7))
-		strip.setPixelColor( i - 2, Color(a * .7 * .7, b * .7 * .7, c * .7 * .7))
-    	strip.setPixelColor( i - 3, Color(a * .7 * .7 * .7, b * .7 * .7 * .7, c * .7 * .7 * .7))
-    	strip.setPixelColor( i - 4, Color(a * .7 * .7 * .7 * .7, b * .7 * .7 * .7 * .7, c * .7 * .7 * .7 * .7))
-    	strip.setPixelColor( i - 5, Color(a * .7 * .7 * .7 * .7 * .7, b * .7 * .7 * .7 * .7 * .7, c * .7 * .7 * .7 * .7 * .7))
-    	strip.setPixelColor( i - 6, Color(a * .7 * .7 * .7 * .7 * .7 * .7, b * .7 * .7 * .7 * .7 * .7 * .7, c * .7 * .7 * .7 * .7 * .7 * .7))
-		strip.show()
-    	strip.setPixelColor( i -7, Color(0,0,0))
+		strip.setPixelColor( i -7, Color(0,0,0))
 		strip.show()
 
-
+def burst(strip, a, b, c):
+	for i in range(90, 119, 1):
+		strip.setPixelColor( i , Color(a, b, c))
+		strip.setPixelColor( i - 1, Color(round(a * .7),round( b * .7 ),round(c * .7)))
+		strip.setPixelColor( i - 2, Color(round(a * .7 * .7), round(b * .7 * .7), round(c * .7 * .7)))
+		strip.setPixelColor( i - 3, Color(round(a * .7 * .7 * .7), round(b * .7 * .7 * .7), round(c * .7 * .7 * .7)))
+		strip.setPixelColor( i - 4, Color(round(a * .7 * .7 * .7 * .7), round(b * .7 * .7 * .7 * .7), round(c * .7 * .7 * .7 * .7)))
+		strip.setPixelColor( i - 5, Color(round(a * .7 * .7 * .7 * .7 * .7), round(b * .7 * .7 * .7 * .7 * .7), round(c * .7 * .7 * .7 * .7 * .7)))
+		strip.setPixelColor( i - 6, Color(round(a * .7 * .7 * .7 * .7 * .7 * .7), round(b * .7 * .7 * .7 * .7 * .7 * .7), round(c * .7 * .7 * .7 * .7 * .7 * .7)))
+		strip.setPixelColor( i - 7, Color(round(a * .7 * .7 * .7 * .7 * .7 * .7 * .7),round(b * .7 * .7 * .7 * .7 * .7 * .7 * .7),round( c * .7 * .7 * .7 * .7 * .7 * .7 * .7)))
+		strip.setPixelColor( i - 8, Color(round(a * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7),round(b * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7),round(c * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7)))
+		strip.setPixelColor( i - 9, Color(round(a * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7),round(b * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7),round(c * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7 * .7)))
+		strip.show()
+		strip.setPixelColor( i -10, Color(0,0,0))
+		strip.show()
 
 # Main program logic follows:
 if __name__ == '__main__':
@@ -92,21 +53,40 @@ if __name__ == '__main__':
 	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 	# Intialize the library (must be called once before other functions).
 	strip.begin()
-
 	print('Press Ctrl-C to quit.')
 	while True:
-		# Color wipe animations.
-		colorWipe(strip, Color(255, 0, 0))  # Red wipe
-		colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-		colorWipe(strip, Color(0, 0, 255))  # Green wipe
-		colorWipe(strip, Color(127, 127, 127))  # White theater chase
-		colorWipe(strip, Color(127,   0,   0))  # Red theater chase
-		colorWipe(strip, Color(  0,   0, 127))  # Blue theater chase
-		# Theater chase animations.
-		theaterChase(strip, Color(127, 127, 127))  # White theater chase
-		theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-		theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-		# Rainbow animations.
-		# rainbow(strip)
-		# rainbowCycle(strip)
-		# theaterChaseRainbow(strip)
+		flare(strip, 0, 0, 255)
+		clean(strip)
+		burst(strip, 0, 0, 255)
+		clean(strip)
+		flare(strip, 0, 255, 0)
+		clean(strip)
+		burst(strip, 0, 255, 0)
+		clean(strip)
+		flare(strip, 255, 0, 0)
+		clean(strip)
+		burst(strip, 255, 0, 0)
+		clean(strip)
+		flare(strip, 0, 255, 255)
+		clean(strip)
+		burst(strip, 255, 0, 255)
+		clean(strip)
+		flare(strip, 255, 0, 255)
+		clean(strip)
+		burst(strip, 255, 250, 0)
+		clean(strip)
+		flare(strip, 255, 250, 0)
+		clean(strip)
+		burst(strip, 50, 255, 255)
+		clean(strip)
+		flare(strip, 50, 255, 255)
+		clean(strip)
+		burst(strip, 255, 50, 150)
+		clean(strip)
+		flare(strip, 255, 50, 150)
+		clean(strip)
+		burst(strip, 255, 250, 50)
+		clean(strip)
+		flare(strip, 255, 250, 50)
+		clean(strip)
+  
