9d31c24b61ab7e6a897f22d6295c39238579d8ff
Mitesh Sharma <mitsharm@redhat.com>
2022-09-12 14:16:57 +0530

    update

---
+++ raspberrypi/02_flag.py
@@ -0,0 +1,68 @@
+# Author: Mitesh Sharma (mitsharm@redhat.com)
+# Name: Simple Fireworks
+# Version: 1.0
+# Description: Simple firewworks show - raspberrypi
+# Program: Python3
+# Library: rpi_ws281x
+
+
+import time
+from rpi_ws281x import * 
+
+# LED strip configuration:
+LED_COUNT      = 120      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0
+
+def clean(strip):
+	for x in range(0, LED_COUNT):
+		strip.setPixelColor(x, Color(0,0,0))
+	strip.show()
+
+def pole(strip, a, b, c):
+	for i in range(0, 89, 1):
+		strip.setPixelColor( i , Color(a, b, c))
+		strip.show()
+
+def white(strip, a, b, c):
+	for i in range(90, 99, 1):
+		strip.setPixelColor( i , Color(a, b, c))
+		strip.show()
+
+def green(strip, a, b, c):
+	for i in range(99, 109, 1):
+		strip.setPixelColor( i , Color(a, b, c))
+		strip.show()
+  
+def saffron(strip, a, b, c):
+	for i in range(109, 119, 1):
+		strip.setPixelColor( i , Color(a, b, c))
+		strip.show()
+
+
+# Main program logic follows:	
+def main():
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	strip.begin()
+	print('Press Ctrl-C to quit.')
+	while True:
+		for x in color_code:
+			pole(strip,x[0],x[1],x[2])
+			green(strip,0, 255, 0)
+			white(strip,255, 255, 255)
+			saffron(strip,244, 196, 48)
+			time.sleep(1)
+			clean(strip)	
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		print('Interrupted')
+		strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+		strip.begin()
+		clean(strip)
--- raspberrypi/test.py
+++ raspberrypi/test.py
@@ -8,7 +8,7 @@ sparkVel = []
 sparkCol = []
 gravity = -.0019
 
-for i in range(0, LED_COUNT):
+for i in range(0, 7):
     sparkPos.insert(i, '')
     sparkVel.insert(i, '')
     sparkCol.insert(i, '')
@@ -18,38 +18,43 @@ def constrain(val, min_val, max_val):
     return min(max_val, max(min_val, val))
 
 
-def flare1():
+def flare1(c):
     flarePos = 0
     flareVel = float(random.getrandbits(16)) / 90
 	# flareVel = float(random16(60, 80)) / 90
     brightness = float(1)
     for i in range(0,5,1):
-        print(flarePos)
-        print(flareVel)
-        print(brightness)
+        # print(flarePos)
+        # print(flareVel)
+        # print(brightness)
         sparkPos[i] = 0
         sparkVel[i] = (float(random.getrandbits(8) / 255) * (flareVel / 5))
         sparkCol[i] = sparkVel[i] * 1000
         sparkCol[i] = constrain(sparkCol[i], 0, 255)
-        print(flarePos)
-        print(flareVel)
-        print(brightness)
-        print(sparkPos)
-        print(sparkVel)
-        print(sparkCol)
-    # while flareVel >= -.05:
-    #     for i in range(0,5,1):
-    #         sparkPos[i] += sparkVel[i]
-    #         sparkPos[i] = constrain(sparkPos[i], 0, 120)
-    #         sparkVel[i] += gravity
-    #         sparkCol[i] += -.8
-    #         sparkCol[i] = constrain(sparkCol[i], 0, 255)
-    #         # strip.setPixelColor(i-s,Color(round(a*pow(.7,s)),round(b*pow(.7,s)),round(c*pow(.7,s))))
-    #         # leds[int(sparkPos[i])] = HeatColor(sparkCol[i])
-    #         # leds[int(sparkPos[i])] %= 50; 
-
-    #     flarePos += flareVel
-    #     flareVel += gravity
-    #     brightness *= .99
-flare1()
+        # print(flarePos)
+        # print(flareVel)
+        # print(brightness)
+        # print(sparkPos)
+        # print(sparkVel)
+        # print(sparkCol)
+    while flareVel >= -.05:
+        for i in range(0,5,1):
+            sparkPos[i] += sparkVel[i]
+            sparkPos[i] = constrain(sparkPos[i], 0, 120)
+            sparkVel[i] += gravity
+            sparkCol[i] += -.8
+            sparkCol[i] = constrain(sparkCol[i], 0, 255)
+            # strip.setPixelColor(i-s,Color(round(a*pow(.7,s)),round(b*pow(.7,s)),round(c*pow(.7,s))))
+            # leds[int(sparkPos[i])] = HeatColor(sparkCol[i])
+            sparkPos[i] %= 50
+            print(round(sparkCol[i]))
+            print(round(sparkPos[i]))
+            c += 1
+            print(str(c) + ' ----------')
+        # break
+        flarePos += flareVel
+        flareVel += gravity
+        brightness *= .99
+c = 0
+flare1(c)
 
