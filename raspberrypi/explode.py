4d91a446353e84b407881d9e4eef957fd403ccdd
Mitesh Sharma <mitsharm@redhat.com>
2022-09-12 15:39:59 +0530

    update

--- raspberrypi/explode.py
+++ raspberrypi/explode.py
@@ -39,17 +39,15 @@ def theaterChase(strip, color, wait_ms=50, iterations=10):
 				strip.setPixelColor(i+q, 0)
     
 def explode0(strip, color, wait_ms=50, iterations=10):
-	for j in range(iterations):
-     
-		for q in range(3):
-			m = random.randrange(strip.numPixels())
-			for i in range(0, 3, 1):
-				strip.setPixelColor(i+m, color)
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, 3, 1):
-				strip.setPixelColor(i+m, 0)
-			strip.show()
+	for q in range(3):
+		m = random.randrange(strip.numPixels())
+		for i in range(0, 6, 1):
+			strip.setPixelColor(i+m, color)
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+		for i in range(0, 6, 1):
+			strip.setPixelColor(i+m, 0)
+		strip.show()
 
 def wheel(pos):
 	"""Generate rainbow colors across 0-255 positions."""
